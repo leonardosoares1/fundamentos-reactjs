a6c9bdda7078c867eaccab76f16f531cb2a78fcf
leonardosoares1 <dev@doleonardo.com.br>
2020-04-23 10:00:07 -0300

    feat: load transcations and import transactions

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -29,13 +29,33 @@ interface Balance {
   total: string;
 }
 
+interface Response {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const response = await api.get<Response>('transactions');
+
+      const transactionsFormatted: Transaction[] = response.data.transactions.map(
+        elem => ({
+          ...elem,
+          formattedValue: `${elem.type === 'outcome' ? '-' : ''} ${formatValue(
+            elem.value,
+          )}`,
+          formattedDate: Intl.DateTimeFormat('pt-BR').format(
+            new Date(elem.created_at),
+          ),
+        }),
+      );
+
+      setTransactions(transactionsFormatted);
+      setBalance(response.data.balance);
     }
 
     loadTransactions();
@@ -51,21 +71,27 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">
+              {formatValue(Number(balance.income))}
+            </h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">
+              {formatValue(Number(balance.outcome))}
+            </h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">
+              {formatValue(Number(balance.total))}
+            </h1>
           </Card>
         </CardContainer>
 
@@ -81,18 +107,14 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(elem => (
+                <tr key={elem.id}>
+                  <td className="title">{elem.title}</td>
+                  <td className={elem.type}>{elem.formattedValue}</td>
+                  <td>{elem.category.title}</td>
+                  <td>{elem.formattedDate}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.forEach(elem => {
+      data.append('file', elem.file, elem.name);
+    });
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const uploadedFile = {
+      file: files[0],
+      name: files[0].name,
+      readableSize: filesize(files[0].size),
+    } as FileProps;
+
+    setUploadedFiles([...uploadedFiles, uploadedFile]);
   }
 
   return (
